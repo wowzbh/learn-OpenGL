c6fe78eb0360e07a37f0ed09ccf61af5f3aa4975
朱博辉 <woozbh@163.com>
2019-07-12 19:27:12 +0800

    test

--- learning/learning/main.cpp
+++ learning/learning/main.cpp
@@ -1,129 +1,134 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <myGL/shader.h>
 #include <iostream>
+#include <myGL/shader.h>
+#include <stb_image.h>
+
+using namespace std;
+
 /*
-	framebuffer 帧缓存器
-	viewport 视口
-	context 上下文
-	Rendering 渲染
-	vertexShader 顶点着色器
-	fragmentShader 片段着色器
-	VAO(vertex array object): 顶点数组对象
-	VBO(vertex buffer object): 顶点缓冲对象
-	EOB(element buffer object) 
-	&IBO(index budffer object): 索引缓冲对象
-	polygon: 多边形
+parameter 参数
 */
 
-//init viewport and viewport size changed with window size change
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-//control of input
 void processInput(GLFWwindow *window);
-
-
 const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 800;
-
-
+const unsigned int SCR_HEIGHT = 600;
 int main()
 {
-	//init a window
 	glfwInit();
-	//set version 3.3 --版本
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	//set Core-profile --核心模式
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	//instantiate window object
-	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "learnOpengl", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Textrue", NULL, NULL);
 	if (window == NULL)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();//end
+		cout << "Filed to creat window" << endl;
 		return -1;
 	}
-	//set context
 	glfwMakeContextCurrent(window);
-	//init viewport
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	//glad: load all OpenGl function pointers
-	//init GLAD
-	//glfwGetProcAddress : load the address of the openGL function that about system 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
+		cout << "Filed to init glad" << endl;
 		return -1;
 	}
 
-	//build and compile shader program
-	//-------------
-	Shader ourShader("shader.vs","shader.fs");
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
-	//set up vertex data and configure vertex attributes
+
+	//渲染
+	Shader ourshader("shader.vs", "shader.fs");
+
 	float vertices[] = {
-	//position             color
-	0.0f, 0.5f, 0.0f,	1.0f, 0.0f, 0.0f,	//up
-	-0.5f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,	//left
-	0.5f, 0.0f, 0.0f,	0.0f, 0.0f, 1.0f	//right
+	//   顶点					颜色					   纹理
+	 0.5f,  0.5f,  0.0f,	1.0f,  0.0f,  0.0f,		1.0f,   1.0f,  //右上 
+	 0.5f, -0.5f,  0.0f,	0.0f,  1.0f,  0.0f,		1.0f,  -1.0f,  //右下
+	-0.5f, -0.5f,  0.0f,	0.0f,  0.0f,  1.0f,	   -1.0f,  -1.0f,  //左下
+	-0.5f,  0.5f,  0.0f,	1.0f,  1.0f,  0.0f,	   -1.0f,   1.0f   //左上
 	};
-	
-	//set up VAO & VBO & EBO
-	unsigned int VAO, VBO;
+
+	unsigned int indices[] = {
+		0, 1, 3,    //first triangle
+		1, 2, 3     //second ...
+	};
+
+	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	//bind the Vertex Array Object first, then bind and set vertex buffer(s), 
-	//and then configure vertex attributes(s).
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	//copy the vertices to buffer
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//set attributes of vertex
-	//position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*) 0);
 	glEnableVertexAttribArray(0);
-	//color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 	
-	//i do not understand why need these code
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-	//render loop
-	//if glfwWindowShouldClose is true exit loop
+
+
+	//loading texture
+
+	//init & bind
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	//set wrap & min_max filter 环绕和放缩
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//nrChannels 颜色通道
+	int width, height, nrChannels;    //texture attribute  
+	unsigned char *data = stbi_load("C:/users/wowzbh/Desktop/container.jpg", &width, &height, &nrChannels, 0);
+
+	//loading 
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		//第二个参数为零，所以自动生成多级渐远纹理
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		cout << "Failed to load Texture" << endl;
+	}
+	stbi_image_free(data);
+
+
+
 	while (!glfwWindowShouldClose(window))
 	{
-		//input 
+		//input
 		processInput(window);
 
-		//Rendering
+		//shadering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-		//draw use line
-		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		//draw triangle
-		ourShader.use();
-		//update uniform color
-		float timeValue = glfwGetTime();
-		float GreenValue = (sin(timeValue)/2.0f) + 0.5f;
-		ourShader.setVec4("ourColor", 0.0f, GreenValue, 0.2f, 1.0f);
-		
-		//draw triangle
+		glBindTexture(GL_TEXTURE_2D, texture);
+		ourshader.use();
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//Swap color buffer
-		//check events
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//end
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
 	glfwTerminate();
 	return 0;
 }
@@ -135,9 +140,8 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 
 void processInput(GLFWwindow *window)
 {
-	//press esc button to close window
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
-		glfwSetWindowShouldClose(window, true);
+		glfwSetWindowShouldClose(window,true);
 	}
 }
