9043407bcd1ed958362078d3d6217415b64ba52d
朱博辉 <woozbh@163.com>
2019-07-14 10:29:14 +0800

    3D box

--- learning/learning/main.cpp
+++ learning/learning/main.cpp
@@ -1,120 +1,160 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <myGL/shader.h>
 #include <stb_image.h>
+#include <myGL/shader.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 using namespace std;
 
-/*
-parameter 参数
-*/
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
 int main()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Textrue", NULL, NULL);
+	
+	GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Matrix", NULL, NULL);
 	if (window == NULL)
 	{
-		cout << "Filed to creat window" << endl;
+		cout << "Failed to creat a window" << endl;
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		cout << "Filed to init glad" << endl;
+		cout << "Failed to init glad" << endl;
 		return -1;
 	}
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
-
-	//渲染
-	Shader ourshader("shader.vs", "shader.fs");
+	Shader ourShader("shader.vs", "shader.fs");
 
 	float vertices[] = {
-	//   顶点					颜色					   纹理
-	 0.5f,  0.5f,  0.0f,	1.0f,  0.0f,  0.0f,		1.0f,   1.0f,  //右上 
-	 0.5f, -0.5f,  0.0f,	0.0f,  1.0f,  0.0f,		1.0f,	0.0f,  //右下
-	-0.5f, -0.5f,  0.0f,	0.0f,  0.0f,  1.0f,		0.0f,   0.0f,  //左下
-	-0.5f,  0.5f,  0.0f,	1.0f,  1.0f,  0.0f,	    0.0f,   1.0f   //左上
-	};
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
 	unsigned int indices[] = {
-		0, 1, 3,    //first triangle
-		1, 2, 3     //second ...
+		 0, 1, 3, // first triangle
+		 1, 2, 3  // second triangle
 	};
 
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
 
-	glBindVertexArray(VAO);
+	glBindVertexArray (VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),vertices, GL_STATIC_DRAW);
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*) 0);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void *)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
 	
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6 * sizeof(float)));
+
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
+	unsigned int texture1, texture2;
 
-
-	//loading texture
-
-	//init & bind
-	unsigned int texture1, texture3;
 	glGenTextures(1, &texture1);
 	glBindTexture(GL_TEXTURE_2D, texture1);
-	//set wrap & min_max filter 环绕和放缩
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	//nrChannels 颜色通道
-	int width, height, nrChannels;    //texture attribute  
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char *data = stbi_load("C:\\users\\wowzbh\\Desktop\\container.jpg", &width, &height, &nrChannels, 0);
 
-	//loading 
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+
+	unsigned char *data = stbi_load("C:\\Users\\wowzbh\\Desktop\\container.jpg", &width, &height, &nrChannels, 0);
+
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-		//第二个参数为零，所以自动生成多级渐远纹理
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}
 	else
 	{
-		cout << "Failed to load Texture" << endl;
+		cout << "Texture load failed" << endl;
 	}
 	stbi_image_free(data);
 
-	glGenTextures(1, &texture3);
-	glBindTexture(GL_TEXTURE_2D, texture3);
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-	data = stbi_load("C:\\users\\wowzbh\\Desktop\\awesomeface.png", &width, &height, &nrChannels, 0);
+	data = stbi_load("C:\\Users\\wowzbh\\Desktop\\awesomeface.png", &width, &height, &nrChannels, 0);
 	if (data)
 	{
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
@@ -122,33 +162,56 @@ int main()
 	}
 	else
 	{
-		cout << "Failed to load texture" << endl;
+		cout << "Texture load failed" << endl;
 	}
 	stbi_image_free(data);
 
-	ourshader.use();
-	glUniform1i(glGetUniformLocation(ourshader.ID,"texture1"), 0);
-	ourshader.setInt("texture2",1);
+	
+	ourShader.use();
+	
+	glUniform1i(glGetUniformLocation(ourShader.ID, "Texture1"), 0);
+	ourShader.setInt("Texture2", 1);
+	
+	glEnable(GL_DEPTH_TEST);
 
 
 	while (!glfwWindowShouldClose(window))
 	{
-		//input
 		processInput(window);
 
-		//shadering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, texture3);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		
+		//view matrix
+		glm::mat4 view = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		//projection matrix
+		glm::mat4 projection = glm::mat4(1.0f);
+		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
-		ourshader.use();
+		int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+
+		ourShader.use();
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		ourShader.setMat4("projection", projection);
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		//end
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			//model matrix
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.f * i;
+			model = glm::rotate(model, (float)glfwGetTime()*glm::radians(angle), glm::vec3(0.5f, 1.0f, 0.0f));
+			ourShader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+
+
+		}
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -165,11 +228,10 @@ void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
-
 void processInput(GLFWwindow *window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 	{
-		glfwSetWindowShouldClose(window,true);
+		glfwSetWindowShouldClose(window, true);
 	}
 }
--- learning/learning/shader.fs
+++ learning/learning/shader.fs
@@ -1,11 +1,12 @@
 #version 330 core
 out vec4 FragColor;
-in vec3 ourColor;
+
 in vec2 TexCoord;
 
-uniform sampler2D ourTexture1;
-uniform sampler2D texture2;
+uniform sampler2D Texture1;
+uniform sampler2D Texture2;
+
 void main()
 {
-	FragColor = mix(texture(ourTexture1, TexCoord),texture(texture2, TexCoord),  0.2);
+	FragColor = mix(texture(Texture1, TexCoord), texture(Texture2, TexCoord), 0.2);
 }
